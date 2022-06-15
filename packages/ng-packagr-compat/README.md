e914c9997ac40eea191f781c70a26f1f1864b0d9
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2022-06-15 16:17:27 +0100

    initial impl

---
+++ packages/ng-packagr-compat/README.md
@@ -0,0 +1,11 @@
+# ng-packagr-compat
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build ng-packagr-compat` to build the library.
+
+## Running unit tests
+
+Run `nx test ng-packagr-compat` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/ng-packagr-compat/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'ng-packagr-compat',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/ng-packagr-compat',
+};
